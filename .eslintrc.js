3ca6d2cb698eb65acebd8abe528a91a5ef3a2fe1
sumtwelve <sumtwelve@gmail.com>
2023-06-06 23:49:15 -0600

    configure eslint and jsconfig

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "extends": [
+        "plugin:vue/recommended",
+        "prettier"
+    ],
+    "parser": "vue-eslint-parser",
+    "parserOptions": {
+        "ecmaVersion": 2015
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+}
