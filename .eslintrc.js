ca76806338b796b9847fbd52dc3013958117e448
sumtwelve <sumtwelve@gmail.com>
2023-06-07 00:06:18 -0600

    fix 'unexpected token import' error

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,11 +9,38 @@ module.exports = {
     ],
     "parser": "vue-eslint-parser",
     "parserOptions": {
-        "ecmaVersion": 2015
+        "ecmaVersion": 2015,
+        "parser": 'babel-eslint',
+        "sourceType": 'module',
+    },
+    settings: {
+        'import/resolver': {
+            webpack: {
+                config: 'node_modules/@vue/cli-service/webpack.config.js',
+            },
+            node: {
+                extensions: ['.js', '.jsx', '.vue'],
+                moduleDirectory: ['node_modules', 'src'],
+            },
+            alias: {
+                map: [['@', './src']],
+                extensions: ['.js', '.vue', '.json'],
+            },
+        },
     },
     "plugins": [
-        "vue"
+        "vue",
+        "import"
     ],
     "rules": {
+        "import/no-unresolved": "error",
+        "import/extensions": [
+            "error",
+            "always",
+            {
+                "js": "never",
+                "vue": "never"
+            }
+        ]
     }
 }
