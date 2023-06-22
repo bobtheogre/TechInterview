9f43554bf74fa42864ba525454d1fe885fb01bb1
David Rasmussen <david.ras97@gmail.com>
2023-06-21 18:58:36 -0600

    Populated table, made it searchable, fit it to the container width

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -11,7 +11,21 @@
             below with headers and make it searchable. Also, expand the table to full width of the container. Let Bob know when complete.
           </p>
         </v-card-subtitle>
-        <v-data-table />
+        <v-data-table
+          width=100%
+          overflow-x="auto"  
+          :search="search"
+          :items="tableData"
+          :headers="headers"
+        >
+        <template v-slot:top>
+          <v-text-field
+            v-model="search"
+            label="Search"
+            class="mx-4"
+          ></v-text-field>
+        </template>
+        </v-data-table> 
       </v-card>
     </v-row>
     <v-row />
@@ -19,9 +33,30 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
 
   data: () => ({
+    tableData: [],
+    headers: [
+      { text: 'First Name', value: 'first_name' },
+      { text: 'Last Name', value: 'last_name' },
+      { text: 'Email', value: 'email' },
+      { text: 'Phone Number', value: 'phone_number'},
+      { text: 'Birth Date', value: 'date_of_birth'}
+    ],
+    search: '',
   }),
+
+    created() {
+      axios.get('https://random-data-api.com/api/v2/users/?size=20')
+        .then(response => {
+          this.tableData = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
 };
 </script>
